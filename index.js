d7ac08eca163359989b07fd63912aec759dc0d3c
axiu <abawor200@gmail.com>
2023-11-15 22:29:25 +0000

    Added reset button.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
-let saveBtn = document.getElementById("save-btn")
-let incrementBtn = document.getElementById("increment-btn")
+const countEl = document.getElementById("count-el")
+const saveEl = document.getElementById("save-el")
+const saveBtn = document.getElementById("save-btn")
+const incrementBtn = document.getElementById("increment-btn")
+const resetBtn = document.getElementById("reset-btn")
 
 
 let count = 0
@@ -9,17 +10,17 @@ let count = 0
 incrementBtn.addEventListener("click", function() {
     count += 1;
     countEl.innerText = count;
-    if ((count % 2) != 0) {
-        incrementBtn.style.backgroundColor = "darkblue";
-    } else {
-        incrementBtn.style.backgroundColor = "darkred";
-}});
+});
 
-
-saveBtn.addEventListener("click", function(){
+saveBtn.addEventListener("click", function() {
     entries = count + " - ";
     saveEl.textContent += entries;
     count = 0;
     countEl.innerText = count;
-    incrementBtn.style.backgroundColor = "darkred";
 });
+
+resetBtn.addEventListener("dblclick", function() {
+    count = 0
+    countEl.textContent = 0
+    countEl.saveEl = "Previous entries: "
+})
